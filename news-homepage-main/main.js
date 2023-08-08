9bb22246c869dc3207b652bd38d4c57a17214464
freaky4wrld <freaky4wrld@gmail.com>
2023-08-08 13:17:07 +0530

    adding the overlay styling to the element beneath the sidebar menu

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -1,9 +1,12 @@
 const menu_btn = document.querySelector('.menu-btn');
 const ul = document.querySelector('.nav-options');
-const close_btn = document.querySelector('.show-btn')
+const close_btn = document.querySelector('.show-btn');
+const overlay_bg = document.querySelector('.overlay');
 menu_btn.addEventListener('click', () =>{
     ul.classList.toggle('show');
+    overlay_bg.classList.toggle('enabled');
 })
 close_btn.addEventListener('click', () =>{
     ul.classList.toggle('show');
+    overlay_bg.classList.toggle('enabled');
 })
