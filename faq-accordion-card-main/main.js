ce9461cb2cbfcccadc517085b7a990003c05e578
unknown <freaky4wrld@gmail.com>
2023-08-15 15:22:18 +0530

    Mobile responsiveness is added to the project

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -6,6 +6,6 @@ btnIcon.forEach((btn) =>{
         let answerToOpenElement = document.querySelector(`.${answerToOpen}`);
         answerToOpenElement.classList.toggle('show');
         btn.classList.toggle('rotate');
-        faqSection.style.padding = '2rem 3rem 2rem 0';
+        // faqSection.style.padding = '2rem 3rem 2rem 0';
     })
 })
