1a16a45bcc5271ef7b22d0b95394c175f576e820
freaky4wrld <freaky4wrld@gmail.com>
2023-08-20 11:38:56 +0530

    added card animations and dismiss button message functionality to go back to the sign-up form

--- newsletter-sign-up-with-success-message-main/flip.js
+++ newsletter-sign-up-with-success-message-main/flip.js
@@ -4,6 +4,7 @@ const formSubmitElement = document.getElementById('form');
 const signUpCard = document.getElementById('sign-up-form');
 const succesMsseageCard = document.getElementById('success-mssg');
 const successEmailElement = document.getElementById('email-response');
+const dismissBtn = document.querySelector('button[type="button"]');
 emailSection.addEventListener('input', ()=>{
     resetEmailValidation();
     if(!emailSection.validity.valid){
@@ -29,6 +30,8 @@ formSubmitElement.addEventListener('invalid', (event)=>{
 
 }, true);
 
+dismissBtn.addEventListener('click', successMessageHide);
+
 
 function resetEmailValidation(){
     validationMessage.innerText = '';
@@ -42,8 +45,12 @@ function successMessageShow(){
     succesMsseageCard.style.display = 'flex';
     signUpCard.style.display = 'none';
     successEmailElement.innerText = emailSection.value;
+    succesMsseageCard.classList.add('animate__animated', 'animate__backInDown');
 }
 function successMessageHide(){
     succesMsseageCard.style.display = 'none';
-    signUpCard.style.display = 'block';
+    signUpCard.style.display = 'flex';
+    signUpCard.classList.add('animate__animated', 'animate__backInLeft');
+    resetEmailValidation()
+    emailSection.value = '';
 }
